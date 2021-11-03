a29eb9956b21917c9e872b0da5f7f9cf4ae4f3aa
Cosmin Mihai Pascariu <cosmin.pascariu2@student.usv.ro>
2021-11-03 22:20:46 +0200

    Solve Lab4 exercises

---
+++ pascariu-cosmin-3134A/Axes.cs
@@ -0,0 +1,37 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace pascariu_cosmin_3134A
+{
+    class Axes // Clasa ce realizeaza desenarea axelor Ox,Oy si Oz
+    {
+        public const int XYZ_SIZE = 25;
+
+        public void Draw()
+        {
+            GL.LineWidth(2.0f);
+
+            GL.Begin(PrimitiveType.Lines);
+            // Desenează axa Ox
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Color3(Color.Blue);
+            GL.Vertex3(XYZ_SIZE, 0, 0);
+            // Desenează axa Oy.
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Color3(Color.Blue);
+            GL.Vertex3(0, XYZ_SIZE, 0);
+            // Desenează axa Oz.
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Color3(Color.Blue);
+            GL.Vertex3(0, 0, XYZ_SIZE);
+            GL.End();
+        }
+    }
+}
---
+++ pascariu-cosmin-3134A/Window.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace pascariu_cosmin_3134A
+{
+    class Window : GameWindow
+    {
+        private CreateCube cube;
+        private Axes axes;
+       
+        private Color color1, color2, color3;
+
+        public Window() : base(800, 600, new GraphicsMode(32, 24, 0, 8))
+        {
+            VSync = VSyncMode.On;
+
+            Console.WriteLine("OpenGl versiunea: " + GL.GetString(StringName.Version));
+            Title = "OpenGl versiunea: " + GL.GetString(StringName.Version);
+
+            axes = new Axes();
+            cube = new CreateCube("./../../date.txt");
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color.FromArgb(1, 30, 30, 30));
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+
+            double aspect_ratio = Width / (double)Height;
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+
+            Matrix4 lookat = Matrix4.LookAt(10, 10, 10, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            KeyboardState keyboard = Keyboard.GetState();
+            MouseState mouse = Mouse.GetState();
+
+            cube.SetColor();
+
+
+            if (keyboard[Key.Escape])
+            {
+                Exit();
+            }
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            axes.Draw();
+
+            cube.Draw();
+
+            SwapBuffers();
+
+        }
+    }
+}
